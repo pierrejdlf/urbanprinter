a1fc7786163188de65829c0ea35028a1fec9e43c
Pierre JdlF <pierre.jdlf@gmail.com>
2015-12-12 01:49:18 +0100

    now

--- urbanPrinter/urbanPrinter.ino
+++ urbanPrinter/urbanPrinter.ino
@@ -11,11 +11,11 @@ PS2Keyboard keyboard;
 // PRINTER pins
 #define DOT0 A0
 #define DOT1 A1
-#define DOT2 10
-#define DOT3 11
-#define DOT4 A5
+#define DOT2 A2
+#define DOT3 A3
+#define DOT4 A4
 
-// I killed A2,A3,A4 ... lack of light !
+// I killed A2,A3,A4 ... lack of light ! (on the mini board)
 
 int PRINTPIN[] = {
   DOT0, DOT1, DOT2, DOT3, DOT4
@@ -29,33 +29,32 @@ LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 // and schema
 
 ///////////////////////////////////////////////////
-//B00011111, B00000101, B00000101, B00000101, B00011111 };
-byte a[5] = { B00011111, B00001001, B00001001, B00001001, B00011111 };
-byte b[5] = { B00011111, B00010100, B00010100, B00010100, B00011100 };
-byte c[5] = { B00011111, B00010001, B00010001, B00010001, B00010001 };
-byte d[5] = { B00011100, B00010100, B00010100, B00010100, B00011111 };
-byte e[5] = { B00011111, B00010101, B00010101, B00010101, B00010001 };
-byte f[5] = { B00011111, B00000101, B00000101, B00000101, B00000001 };
-byte g[5] = { B00011111, B00010001, B00010001, B00010101, B00011101 };
-byte h[5] = { B00011111, B00000100, B00000100, B00000100, B00011111 };
-byte i[5] = { B00010001, B00010001, B00011111, B00010001, B00010001 };
-byte j[5] = { B00010000, B00010000, B00010001, B00011111, B00010001 };
-byte k[5] = { B00011111, B00000100, B00001110, B00011011, B00010001 };
-byte l[5] = { B00011111, B00010000, B00010000, B00010000, B00010000 };
-byte m[5] = { B00011111, B00000011, B00001100, B00000011, B00011111 };
-byte n[5] = { B00011111, B00000011, B00001110, B00011000, B00011111 };
-byte o[5] = { B00011111, B00010001, B00010001, B00010001, B00011111 };
-byte p[5] = { B00011111, B00000101, B00000101, B00000101, B00000111 };
-byte q[5] = { B00000111, B00000101, B00000101, B00000101, B00011111 };
-byte r[5] = { B00011111, B00000101, B00001101, B00010111, B00010000 };
-byte s[5] = { B00010111, B00010101, B00010101, B00010101, B00011101 };
-byte t[5] = { B00000001, B00000001, B00011111, B00000001, B00000001 };
-byte u[5] = { B00011111, B00010000, B00010000, B00010000, B00011111 };
-byte v[5] = { B00000011, B00011100, B00010000, B00011100, B00000011 };
-byte w[5] = { B00011111, B00001000, B00001100, B00001000, B00011111 };
-byte x[5] = { B00010001, B00001010, B00000100, B00001010, B00010001 };
-byte y[5] = { B00000111, B00000100, B00010100, B00010100, B00011111 };
-byte z[5] = { B00010001, B00001000, B00000100, B00000010, B00010001 };
+const byte a[5] = { B00011111, B00000101, B00000101, B00000101, B00011111 };
+const byte b[5] = { B00011111, B00010100, B00010100, B00010100, B00011100 };
+const byte c[5] = { B00011111, B00010001, B00010001, B00010001, B00010001 };
+const byte d[5] = { B00011100, B00010100, B00010100, B00010100, B00011111 };
+const byte e[5] = { B00011111, B00010101, B00010101, B00010101, B00010001 };
+const byte f[5] = { B00011111, B00000101, B00000101, B00000101, B00000001 };
+const byte g[5] = { B00011111, B00010001, B00010001, B00010101, B00011101 };
+const byte h[5] = { B00011111, B00000100, B00000100, B00000100, B00011111 };
+const byte i[5] = { B00010001, B00010001, B00011111, B00010001, B00010001 };
+const byte j[5] = { B00010000, B00010000, B00010001, B00011111, B00010001 };
+const byte k[5] = { B00011111, B00000100, B00001110, B00011011, B00010001 };
+const byte l[5] = { B00011111, B00010000, B00010000, B00010000, B00010000 };
+const byte m[5] = { B00011111, B00000011, B00001100, B00000011, B00011111 };
+const byte n[5] = { B00011111, B00000011, B00001110, B00011000, B00011111 };
+const byte o[5] = { B00011111, B00010001, B00010001, B00010001, B00011111 };
+const byte p[5] = { B00011111, B00000101, B00000101, B00000101, B00000111 };
+const byte q[5] = { B00000111, B00000101, B00000101, B00000101, B00011111 };
+const byte r[5] = { B00011111, B00000101, B00001101, B00010111, B00010000 };
+const byte s[5] = { B00010111, B00010101, B00010101, B00010101, B00011101 };
+const byte t[5] = { B00000001, B00000001, B00011111, B00000001, B00000001 };
+const byte u[5] = { B00011111, B00010000, B00010000, B00010000, B00011111 };
+const byte v[5] = { B00000011, B00011100, B00010000, B00011100, B00000011 };
+const byte w[5] = { B00011111, B00001000, B00001100, B00001000, B00011111 };
+const byte x[5] = { B00010001, B00001010, B00000100, B00001010, B00010001 };
+const byte y[5] = { B00000111, B00000100, B00010100, B00010100, B00011111 };
+const byte z[5] = { B00010001, B00001000, B00000100, B00000010, B00010001 };
 
 const byte* alph[26] = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z };
 
@@ -78,18 +77,16 @@ int charP = 0;
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
+  
   // PRINTER
   pinMode(DOT0, OUTPUT);
   pinMode(DOT1, OUTPUT);
   pinMode(DOT2, OUTPUT);
   pinMode(DOT3, OUTPUT);
   pinMode(DOT4, OUTPUT);
-  digitalWrite(DOT0, LOW);
-  digitalWrite(DOT1, LOW);
-  digitalWrite(DOT2, LOW);
-  digitalWrite(DOT3, LOW);
-  digitalWrite(DOT4, LOW);
-
+  
+  closeVannes();
+  
   // TRIGGER
   pinMode(TRIGGER, INPUT);
 
@@ -101,17 +98,40 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("Against bush !");
 
-  Serial.println("Ok. I'm ready Bush");
+  //Serial.println("Ok. I'm ready Bush");
   delay(100);
-
-  //startPrinting(sentence);
 }
+
+boolean debug = true;
+int loup = 0;
 
 void loop() {
 
-  delay(1);
-  
+  delay(10);
 
+  if(debug) {
+    if(shouldgo()) {
+      Serial.println("GOTEST");
+      if((loup%2)==0) {
+        analogWrite(PRINTPIN[0], 255);
+        analogWrite(PRINTPIN[1], 0);
+        analogWrite(PRINTPIN[2], 255);
+        analogWrite(PRINTPIN[3], 0);
+        analogWrite(PRINTPIN[4], 255);
+      } else {
+        analogWrite(PRINTPIN[0], 0);
+        analogWrite(PRINTPIN[1], 255);
+        analogWrite(PRINTPIN[2], 0);
+        analogWrite(PRINTPIN[3], 255);
+        analogWrite(PRINTPIN[4], 0);
+      }
+    } else {
+      closeVannes();
+      loup = loup+1;
+    }
+    
+  }
+  
   //// keyboard char receive loop
   if (keyboard.available()) {
     // read the next key
@@ -127,7 +147,7 @@ boolean shouldgo() {
 void addCharToMemory(char ch) {
   sentenceSet[charP] = ch;
   charP += 1;
-  Serial.println(sentenceSet);
+  //Serial.println(sentenceSet);
   lcd.setCursor(0, 1);
   lcd.print(sentenceSet);
 }
@@ -153,7 +173,7 @@ void receiveChar(char ch) {
   }
   if (ch == PS2_ENTER) { // NEWLINE || ch==PS2_DOWNARROW
     lcd.clear();
-    Serial.println("GO PRINT !");
+    //Serial.println("GO PRINT !");
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("ETAT d'URGENCE");
@@ -171,10 +191,10 @@ void startPrinting(char* sent, int l) {
   if (l)
     len = l + 1;
 
-  Serial.print("print: ");
-  Serial.println(sent);
-  Serial.print("size is: ");
-  Serial.println(l);
+  //Serial.print("print: ");
+  //Serial.println(sent);
+  //Serial.print("size is: ");
+  //Serial.println(l);
 
   for (int i = 0; i < l; i = i + 1) {
     printLetter(sent[i]);
@@ -186,7 +206,7 @@ void startPrinting(char* sent, int l) {
 
 void closeVannes() {
   for (int dot = 0; dot < 5; dot = dot + 1) {
-    digitalWrite(PRINTPIN[dot], LOW);
+    analogWrite(PRINTPIN[dot], 0);
   }
 }
 
@@ -202,29 +222,38 @@ void waitLetter() { // between letters
   delay(2000);
 }
 void printLetter(char c) {
-  Serial.print("print letter: ");
-  Serial.println(c);
+  //Serial.print("print letter: ");
+  //Serial.println(c);
   int w = (int)c - 97;
-  //Serial.println(w);
+  Serial.println(w);
   const byte *charlist = alph[w];
-
+  lcd.setCursor(15,0);
+  lcd.print(String(c));
+  
   // for each line
   for (int vert = 0; vert < 5; vert = vert + 1) {
-    lcdPrint((String)vert);
+
+    //lcd.setCursor(15,0);
+    lcd.setCursor(0,1);
+    lcd.print(String(vert));
+    lcd.print(" ");
     if (!config) {
       // for each dot
-      Serial.println("vertically");
-      for (int dot = 0; dot < 5; dot = dot + 1) {
-        //charint is from keyboard
-        //boolean on = alph[charint-97];
-        boolean on = charlist[vert] & (1 << dot);
-        Serial.println(on ? "X" : "O");
-        if (on) {
-          //if(shouldgo())
-          digitalWrite(PRINTPIN[dot], HIGH);
+      
+      for (int pix = 0; pix < 5; pix = pix + 1) {
+        
+        boolean on = charlist[vert] & (1 << pix);
+        
+        //Serial.println(on ? "X" : "O");
+
+        if(on>0) {
+          analogWrite(PRINTPIN[pix], 255);
+          lcd.print("X");
         }
-        else
-          digitalWrite(PRINTPIN[dot], LOW);
+        else {
+          analogWrite(PRINTPIN[pix], 0);
+          lcd.print("_");
+        }
       }
       waitLine();
     }
